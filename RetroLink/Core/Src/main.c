013c9be2f6290c0f8e43ec59e9410ce42a427565
Masaki Chien <masa.chien@gmail.com>
2026-07-08 20:59:53 -0700

    feat: CAN loopback verified - ADC + CAN TX working
    - Added CAN task code (500kbps, ID 0x100, 4-byte payload)
    - Configured CAN loopback mode with corrected baud rate (Prescaler=2, BS1=13, BS2=2)
    - Fixed CAN init failure: added GPIO_PULLUP on PB8 (CAN_RX) so peripheral
      sees recessive bits during init-to-normal transition
    - Added CAN RX callback with LED toggle for loopback verification
    - Signal conditioning circuit tested: 3.3V->2.25V, 5V->2.60V, GND->0V
    - Configured PA5 as GPIO output for LD2 LED
    - Enabled CAN1 RX0 NVIC interrupt

--- RetroLink/Core/Src/main.c
+++ RetroLink/Core/Src/main.c
@@ -73,7 +73,6 @@ const osThreadAttr_t displayTask_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityBelowNormal,
 };
-
 /* USER CODE BEGIN PV */
 
 volatile uint16_t adc_raw[2];		// Raw 12-bit ADC readings (0-4095)
@@ -135,6 +134,31 @@ int main(void)
   MX_I2C1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+
+  // Pull-up on CAN_RX (PB8) so the peripheral sees recessive (high)
+  // during the init-to-normal transition
+  GPIO_InitTypeDef canrx_fix = {0};
+  canrx_fix.Pin = GPIO_PIN_8;
+  canrx_fix.Mode = GPIO_MODE_AF_PP;
+  canrx_fix.Pull = GPIO_PULLUP;
+  canrx_fix.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  canrx_fix.Alternate = GPIO_AF9_CAN1;
+  HAL_GPIO_Init(GPIOB, &canrx_fix);
+
+  CAN_FilterTypeDef filter;
+  filter.FilterBank = 0;
+  filter.FilterMode = CAN_FILTERMODE_IDMASK;
+  filter.FilterScale = CAN_FILTERSCALE_32BIT;
+  filter.FilterIdHigh = 0x0000;
+  filter.FilterIdLow = 0x0000;
+  filter.FilterMaskIdHigh = 0x0000;
+  filter.FilterMaskIdLow = 0x0000;
+  filter.FilterFIFOAssignment = CAN_RX_FIFO0;
+  filter.FilterActivation = ENABLE;
+  HAL_CAN_ConfigFilter(&hcan1, &filter);
+
+  HAL_CAN_Start(&hcan1);
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 
   /* USER CODE END 2 */
 
@@ -300,11 +324,11 @@ static void MX_CAN1_Init(void)
 
   /* USER CODE END CAN1_Init 1 */
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 16;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.Prescaler = 2;
+  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
@@ -405,10 +429,28 @@ static void MX_GPIO_Init(void)
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  CAN_RxHeaderTypeDef rx_header;
+  uint8_t rx_data[8];
+  HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data);
+  if (rx_header.StdId == 0x100)
+  {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  }
+}
 
 /* USER CODE END 4 */
 
@@ -425,7 +467,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE END Header_StartSensorTask */
 void StartSensorTask(void *argument)
 {
-	/* USER CODE BEGIN 5 */
+  /* USER CODE BEGIN 5 */
 	/* Infinite loop */
 	for(;;) {
 	  // Channel 0 (PA0) - coolant temperature sensor
@@ -446,7 +488,7 @@ void StartSensorTask(void *argument)
 
 	  osDelay(100);
 	}
-	/* USER CODE END 5 */
+  /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_StartCanTask */
@@ -468,7 +510,7 @@ void StartSensorTask(void *argument)
 /* USER CODE END Header_StartCanTask */
 void StartCanTask(void *argument)
 {
-	/* USER CODE BEGIN StartCanTask */
+  /* USER CODE BEGIN StartCanTask */
 
 	CAN_TxHeaderTypeDef tx_header;
 	uint8_t tx_data[8];
@@ -481,9 +523,6 @@ void StartCanTask(void *argument)
 	tx_header.IDE = CAN_ID_STD;				// Standard 11-bit ID
 	tx_header.DLC = 4;						// 4 bytes of data
 	tx_header.TransmitGlobalTime = DISABLE;
-
-	// Start the CAN peripheral
-	HAL_CAN_Start(&hcan1);
 
 	for (;;) {
 	  // Pack sensor 0 into bytes 0-1 (big-endian)
@@ -499,7 +538,7 @@ void StartCanTask(void *argument)
 
 	  osDelay(200);
 	}
-	/* USER CODE END StartCanTask */
+  /* USER CODE END StartCanTask */
 }
 
 /* USER CODE BEGIN Header_StartDisplayTask */
@@ -511,7 +550,7 @@ void StartCanTask(void *argument)
 /* USER CODE END Header_StartDisplayTask */
 void StartDisplayTask(void *argument)
 {
-	/* USER CODE BEGIN StartDisplayTask */
+  /* USER CODE BEGIN StartDisplayTask */
 	char uart_buf[64];
 
 	for(;;) {
@@ -525,7 +564,7 @@ void StartDisplayTask(void *argument)
 
 		osDelay(500);
 	}
-	/* USER CODE END StartDisplayTask */
+  /* USER CODE END StartDisplayTask */
 }
 
 /**
